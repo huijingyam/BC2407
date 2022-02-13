0c5978549e4911bc7a8394f18fb8b88e772acd9b
Yap Yu Xiang <shawn@Yaps-MacBook-Pro-2.local>
2022-02-13 21:39:37 +0800

    Added association rules performed on factor variable. Allows association rules on both true and false items.

--- dataPrep_assocRules.R
+++ dataPrep_assocRules.R
@@ -12,6 +12,7 @@ symptoms = data.frame(lapply(symptoms, as.logical))
 
 # Convert to transactions datatype
 symptomsTrans <- as(symptoms, "transactions")  
+symptomsTrans.df<-as(symptomsTrans,"data.frame")
 inspect(symptomsTrans)
 
 
@@ -46,9 +47,41 @@ rules2.clean = rules2[!is.redundant(rules2)]
 rules2.clean.df = as(rules2.clean, "data.frame")
 inspect(head(rules2.clean, n = 10, decreasing = FALSE, by ="count"))
 
+#-----Trying with wide data format with factor variables
+symptomsFac<-data.frame(symptoms)
+symptomsFac[] <- lapply(symptomsFac, factor)
+symptomsTransFac <- as(symptomsFac, "transactions")  
+symptomsTransFac.df<-as(symptomsTransFac,"data.frame")
+inspect(symptomsTransFac)
+# Generating rules at support 0.1: 72,422,843 rules
+rules3 <- apriori(data = symptomsTransFac, parameter = 
+                    list(minlen = 2, supp=0.1, conf = 0.1, target = "rules"))
+summary(rules3)
+inspect(head(rules3, n = 10, by ="lift"))
+rule.table3 <- inspect(rules3)
 
+# cleaning redundant rules from rules1
+# 2,930,561 unredundant rules
+sum(!is.redundant(rules3))
+rules3.clean = rules3[!is.redundant(rules3)]
+rules3.clean.df = as(rules3.clean, "data.frame")
+inspect(head(rules3.clean, n = 10, decreasing = FALSE, by ="count"))
 
+# Generating rules at support 0.1 and conf 0.9: 56,571,092 rules
+rules4 <- apriori(data = symptomsTransFac, parameter = 
+                    list(minlen = 2, supp=0.1, conf = 0.9, target = "rules"))
+summary(rules4)
+inspect(head(rules4, n = 10, by ="lift"))
+rule.table4 <- inspect(rules4)
 
+# cleaning redundant rules from rules1
+# 71,854 unredundant rules
+sum(!is.redundant(rules4))
+rules4.clean = rules4[!is.redundant(rules4)]
+rules4.clean.df = as(rules4.clean, "data.frame")
+inspect(head(rules4.clean, n = 10, decreasing = FALSE, by ="count"))
+# churn_rules<-subset(rules, subset=(rhs %pin% 'rolls/buns')& support >= 0.001 & confidence>=0.65)
+# inspect(churn_rules)
 
 #--------- arulesViz plots -----------#
 
